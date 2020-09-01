e4f5766d67e2ef625a6422b83e24288fd360ff5c
Quoc Ngo <zanengo04@gmail.com>
2020-09-01 18:43:59 -0400

    skeleton added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react'
+import FlashcardList from './FlashcardList'
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <FlashcardList flashcards = {flashcards} />
+  )
+  }
+const SAMPLE_FLASHCARDS =[
+  {
+    id:1,
+    questions: 'What is 2 + 2?',
+    answer: 4,
+    options: ['1','2','3','4']
+  },
+  {
+    id:2,
+    questions: 'Question 2',
+    answer: 4,
+    options: ['1','2','3','4']
+  },
+  {
+    id:3,
+    questions: 'What is 2 + 2?',
+    answer: 4,
+    options: ['1','2','3','4']
+  }
+]
 
-export default App;
+export default App
---
+++ src/Flashcard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Flashcard({flashcard}) {
+    return (
+        <div>
+            {flashcard.question}
+        </div>
+    )
+}
