6eded99de17513b730b3c3c93407f2e01733f19f
Quoc Ngo <zanengo04@gmail.com>
2020-09-02 12:25:58 -0400

    card flip added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React , {useState} from 'react'
+import React , {useState, useEffect} from 'react'
 import FlashcardList from './FlashcardList'
-
+import './app.css'
+import axios from 'axios'
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res =>{
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer =  questionItem.correct_answer
+          const options = [...questionItem.incorrect_answers, answer]
+          return {
+            id: `${index}-${Date.now}`,
+            question: questionItem.question,
+            answer:questionItem.correct_answer,
+            options: options.sort(() => Math.random() - 0.5 )
+
+          }
+        }))
+      })  
+  }, [])
   return (
     <FlashcardList flashcards = {flashcards} />
   )
-  }
+}
 const SAMPLE_FLASHCARDS =[
   {
     id:1,
-    questions: 'What is 2 + 2?',
+    question: 'What is 2 + 2?',
     answer: 4,
     options: ['1','2','3','4']
   },
   {
     id:2,
-    questions: 'Question 2',
+    question: 'Question 2',
     answer: 4,
     options: ['1','2','3','4']
   },
   {
     id:3,
-    questions: 'What is 2 + 2?',
+    question: 'Question 3',
     answer: 4,
     options: ['1','2','3','4']
   }
--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
 export default function Flashcard({flashcard}) {
+    const [flip, setFlip] = useState(false)
+
+
     return (
-        <div>
-            {flashcard.question}
+        <div 
+        //dynamic classes
+        className = {`card ${flip ? 'flip' : ''}`}
+        onClick={() => setFlip(!flip)}>
+
+            <div className = 'front'>
+                {flashcard.question}
+                <div className = "flasscard-options">
+                    {flashcard.options.map(option => {
+                        return <div className = "flashcard-option">{option}</div>
+                    })}
+                </div>
+            </div>
+            <div className = 'back'>{flashcard.answer}</div>
+            
         </div>
     )
 }
