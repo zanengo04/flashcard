1940274771f23361ce6999049a5b288fe95964ad
Quoc Ngo <zanengo04@gmail.com>
2020-12-30 16:55:47 -0500

    Updates

---
+++ precache-manifest.b54b3386d40ed42768e548dce7c3b6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8719bcbc20b07612ca2a4658988ec1f2",
+    "url": "/flashcard/index.html"
+  },
+  {
+    "revision": "cda1530abec2ff9bed7f",
+    "url": "/flashcard/static/css/main.153a1431.chunk.css"
+  },
+  {
+    "revision": "0fe55c1d3ceb5814f36d",
+    "url": "/flashcard/static/js/2.5cb33fd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashcard/static/js/2.5cb33fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cda1530abec2ff9bed7f",
+    "url": "/flashcard/static/js/main.0b24174a.chunk.js"
+  },
+  {
+    "revision": "8e5e6aad4f916ce572ad",
+    "url": "/flashcard/static/js/runtime-main.d615c48f.js"
+  }
+]);
